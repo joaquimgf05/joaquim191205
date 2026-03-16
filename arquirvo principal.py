c6774a18de2327e16ae01144d2efe6ec5998c118
JOAQUIM GOMES FARIAS <Joaquimgf05@gmail.com>
2026-03-16 14:53:29 -0300

    Add files via upload

--- arquirvo principal.py
+++ arquirvo principal.py
@@ -55,3 +55,58 @@ aluno1.mostrar_dados()
 
 print("\n--- Dados do Aluno 2 ---")
 aluno2.mostrar_dados()
+
+class AlunoIntegrado(Aluno):
+    def __init__(self, nome, matricula, curso):
+        super().__init__(nome, matricula)
+        self.__curso = None
+        self.set_curso_tecnico(curso)
+
+    def monstrar_dados(self):
+        super().mostrar_dados()
+        print(f"Curso Técnico: {self.get_curso_tecnico()}")
+
+class AlunoGraduação(Aluno):
+    def __init__(self, nome, matricula, curso):
+        super().__init__(nome, matricula)
+        self.__curso = None
+        self.set_curso_graduacao(curso)
+
+    def mostrar_dados(self):
+        super().mostrar_dados()
+        print(f"Curso de Graduação: {self.get_curso_graduacao()}")
+
+# Testando o Polimorfismo
+aluno1= AlunoIntegrado("joão","12345678", "Informática")
+aluno2 = AlunoGraduação("Mariana", "98765432", "Sistemas de Informação")
+
+aluno1.adicionar_nota(8)
+aluno1.adicionar_nota(9)
+
+aluno2.adicionar_nota(7)
+aluno2.adicionar_nota(6)
+
+# Polimorfismo: ambos os objetos respondem ao método mostrar_dados() de maneira diferente
+aluno1.mostrar_dados()
+print("---")
+aluno2.mostrar_dados()
+
+class AlunoSubsequente(Aluno):
+    def __init__(self, nome, matricula, curso):
+        super().__init__(nome, matricula)
+        self.__curso = None
+        self.set_curso_subsequente(curso)
+
+    def mostrar_dados(self):
+        super().mostrar_dados()
+        print(f"Curso Subsequente: {self.get_curso_subsequente()}")
+
+class AlunoPosgraduação(Aluno):
+    def __init__(self, nome, matricula, curso):
+        super().__init__(nome, matricula)
+        self.__curso = None
+        self.set_curso_posgraduacao(curso)
+
+    def mostrar_dados(self):
+        super().mostrar_dados()
+        print(f"Curso de Pós-Graduação: {self.get_curso_posgraduacao()}")
